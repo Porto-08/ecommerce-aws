1ff31ad4faba062487126a88068b2afb1144318c
Samuel Porto <samuelalcala2001@outlook.com>
2022-09-29 09:52:26 -0300

    feat(cancelImportFunction): Cancelamento do processamento da nota fiscal

--- lambda/invoices/cancelImportFunction.ts
+++ lambda/invoices/cancelImportFunction.ts
@@ -1,14 +1,57 @@
-import { APIGatewayProxyResult } from 'aws-lambda';
-import { APIGatewayProxyEvent, Context } from 'aws-lambda';
-import * as AWSRay from 'aws-xray-sdk';
+import { InvoiceTransactionStatus } from '/opt/nodejs/invoiceTransaction';
+import { InvoiceTransationRepository } from '/opt/nodejs/invoiceTransaction';
+import { APIGatewayProxyEvent, APIGatewayProxyResultV2, Context } from 'aws-lambda';
+import { ApiGatewayManagementApi, DynamoDB } from 'aws-sdk';
+import * as AWSXRay from 'aws-xray-sdk';
+import { InvoiceWsService } from '/opt/nodejs/InvoiceWSConection';
+import { InvoiceFile, InvoiceRepository } from '/opt/nodejs/invoiceRepository';
 
-AWSRay.captureAWS(require('aws-sdk'));
+AWSXRay.captureAWS(require('aws-sdk'));
 
-export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
-  console.log('event', event);
+const invoicesDdb = process.env.INVOICE_DDB!;
+const invoicesWSApiEndpoint = process.env.INVOICE_WSAPI_ENDPOINT!.substring(6);
+
+const ddbClient = new DynamoDB.DocumentClient();
+const apiGwManagementApi = new ApiGatewayManagementApi({
+  endpoint: invoicesWSApiEndpoint,
+});
+
+const invoiceTransationRepository = new InvoiceTransationRepository(ddbClient, invoicesDdb);
+const invoiceWsService = new InvoiceWsService(apiGwManagementApi);
+
+export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResultV2> {
+  const transactionId = JSON.parse(event.body!).transactionId as string;
+
+  const lambdaRequestId = context.awsRequestId;
+  const connectionId = event.requestContext.connectionId!;
+
+  console.log(`ConnectionID: ${connectionId} - Lambda Request ID: ${lambdaRequestId}`);
+
+  try {
+    const invoiceTransaction = await invoiceTransationRepository.getInvoiceTransaction(transactionId);
+
+    if (invoiceTransaction.transationStatus === InvoiceTransactionStatus.GENERATED) {
+
+      // Update the transaction status to Cancelled
+      await Promise.all([
+        invoiceWsService.sendInvoiceStatus(transactionId, connectionId, InvoiceTransactionStatus.CANCELED),
+        invoiceTransationRepository.updateInvoiceTransaction(transactionId, InvoiceTransactionStatus.CANCELED),
+      ]);
+    } else {
+      // Send the current status to the client and log the message
+      await invoiceWsService.sendInvoiceStatus(transactionId, connectionId, invoiceTransaction.transationStatus);
+
+      console.error(`Invoice ${transactionId} is already ${invoiceTransaction.transationStatus}, Can't cancel`);
+    }
+  } catch (error) {
+    // Send the error to the client
+    console.error(`Error: ${error}`);
+
+    await invoiceWsService.sendInvoiceStatus(transactionId, connectionId, InvoiceTransactionStatus.NOT_FOUND);
+  }
 
   return {
     statusCode: 200,
-    body: 'OK',
-  };
+    body: JSON.stringify({}),
+  }
 }
